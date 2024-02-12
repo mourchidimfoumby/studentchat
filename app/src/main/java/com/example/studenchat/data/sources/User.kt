5e194c0c6509ad9913d980a83d4273a9445294e2
Mourchidi <mfoumbymourchidi@gmail.com>
2024-02-12 19:19:32 +0100

    Update User.kt

--- app/src/main/java/com/example/studenchat/data/sources/User.kt
+++ app/src/main/java/com/example/studenchat/data/sources/User.kt
@@ -5,6 +5,7 @@ import com.google.firebase.database.Exclude
 import java.io.Serializable
 
 data class User(
+    val uid: String = ""
     val name: String = "",
     val firstname: String = "",
     val mail: String = "",
@@ -13,7 +14,6 @@ data class User(
     @get:Exclude
     val picture: Int = R.drawable.ic_avatar,
 ): Serializable {
-    lateinit var uid: String
     @Exclude
     override fun toString(): String {
         return String.format("%s %s", firstname, name)
