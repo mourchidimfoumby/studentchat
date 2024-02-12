7815257740cc04e0f47a5802f265e392c7d4a3a7
Mourchidi <mfoumbymourchidi@gmail.com>
2024-02-12 19:14:40 +0100

    Create AuthenticationRepository.kt

---
+++ app/src/main/java/com/example/studenchat/authentication/AuthenticationRepository.kt
@@ -0,0 +1,37 @@
+package com.example.studenchat.authentication
+
+import android.util.Log
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class AuthenticationRepository {
+    private val auth = Firebase.auth
+
+    suspend fun logInWithEmailPassword(mail: String, password: String) = suspendCoroutine { continuation ->
+        auth.signInWithEmailAndPassword(mail, password)
+            .addOnSuccessListener {
+                Log.i(javaClass.name, "Connection successful !")
+                continuation.resume(Unit)
+            }
+            .addOnFailureListener { e ->
+                Log.e(javaClass.name, e.message.toString())
+                continuation.resumeWithException(e)
+            }
+    }
+    suspend fun signUpWithEmailPassword(mail: String, password: String) = suspendCoroutine { continuation ->
+        auth.createUserWithEmailAndPassword(mail, password)
+            .addOnSuccessListener {
+                Log.i(javaClass.name, "Registration and Sign-in successful !")
+                continuation.resume(Unit)
+            }
+            .addOnFailureListener { e ->
+                Log.e(javaClass.name, e.message.toString())
+                continuation.resumeWithException(e)
+            }
+    }
+}
