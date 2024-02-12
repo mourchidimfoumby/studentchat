278775dd9cbf1e9564a9f6742031169ef0be19dc
Mourchidi <mfoumbymourchidi@gmail.com>
2024-02-12 19:16:16 +0100

    Update SignUpUseCase.kt

--- app/src/main/java/com/example/studenchat/authentication/domain/SignUpUseCase.kt
+++ app/src/main/java/com/example/studenchat/authentication/domain/SignUpUseCase.kt
@@ -1,31 +1,27 @@
 package com.example.studenchat.authentication.domain
 
 import android.util.Log
-import com.example.studenchat.data.repositories.firebase.FirebaseParameters
-import com.example.studenchat.data.sources.User
+import com.example.studenchat.authentication.AuthenticationRepository
+import com.example.studenchat.data.User
+import com.example.studenchat.repository.FirebaseParameters
+import com.example.studenchat.repository.UserRepository
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import java.lang.IllegalArgumentException
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-class SignUpUseCase : FirebaseParameters {
-    override val tableName = "user"
-    suspend operator fun invoke(user: User) = suspendCoroutine { continuation ->
-        auth.createUserWithEmailAndPassword(user.mail, user.password)
-            .addOnSuccessListener {
-                firebaseDatabase.child(tableName).child(auth.uid.toString()).setValue(user)
-                    .addOnSuccessListener {
-                        Log.i(javaClass.name, "Registration successful !")
-                        continuation.resume(Unit)
-                    }
-                    .addOnFailureListener { e ->
-                        Log.e(javaClass.name, e.message.toString())
-                        continuation.resumeWithException(e)
-                    }
-            }
-            .addOnFailureListener { e ->
-                Log.e(javaClass.name, e.message.toString())
-                continuation.resumeWithException(e)
-            }
+class SignUpUseCase {
+    private val authenticationRepository = AuthenticationRepository()
+    private val userRepository = UserRepository()
+    suspend operator fun invoke(user: User) {
+        if(user.mail.isBlank() || user.password.isBlank()) throw IllegalArgumentException("Fields is blank")
+        else{
+            authenticationRepository.signUpWithEmailPassword(user.mail, user.password)
+            authenticationRepository.logInWithEmailPassword(user.mail, user.password)
+            userRepository.createUser(user)
+        }
     }
 
 }
